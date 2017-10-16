1c851f6a93a08bc83b661601d0f70875bfa682f0
yangzhihang627 <yangzhihang627@qq.com>
2017-10-16 17:49:25 +0800

    complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 // 一些常用路径
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
@@ -8,14 +10,18 @@ var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
 module.exports = {
     entry: {
-        app: path.resolve(APP_PATH, 'app.jsx')
+        // 业务代码
+        bundle: path.resolve(APP_PATH, 'app.jsx'),
+        // 第三方库
+        vendor: ["react", "react-dom", "utils/ajax"]
     },
+    // 输出路径
     output: {
         path: BUILD_PATH,
-        filename: 'bundle.js'
+        filename: './[name].js'
     },
     // 开启dev source map
-    devtool: 'eval-source-map',
+    devtool: false,
     devServer: {
         historyApiFallback: true,
         hot: true,
@@ -34,14 +40,36 @@ module.exports = {
                 include: APP_PATH
             },
             {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
                 test: /\.scss$/,
-                loaders: ['style', 'css', 'sass']
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
             }
         ]
     },
     plugins: [
         new HtmlwebpackPlugin({
             title: 'Deskmark app'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            output: {
+                comments: false // remove all comments
+            },
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+            }
+        }),
+        new ExtractTextPlugin("bundle.css"),
+        //将第三方库打包到vendor.js
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
         })
     ],
     // 可以在js中import jsx文件
